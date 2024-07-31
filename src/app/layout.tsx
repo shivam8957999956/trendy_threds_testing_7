8615749c3d437cf36e3ad2a27dcaaef43e780fd7
shivam8957999956 <084ssharan@gmail.com>
2024-07-31 10:31:16 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,17 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { WixClientContextProvider } from "@/context/wixContext";
 import SaleLive from "@/components/SaleLive";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(`https://www.trendythreds.in`),
   title: "Trendy Threds",
-  description: "A complete e-commerce Store",
+  description:
+    "Explore our luxury clothing brand's exclusive collection of premium t-shirts. Experience unique, high-quality designs crafted for elegance and comfort. Elevate your style with our timeless, sophisticated pieces.",
+  alternates: {
+    canonical: "./",
+  },
 };
 
 export default function RootLayout({
